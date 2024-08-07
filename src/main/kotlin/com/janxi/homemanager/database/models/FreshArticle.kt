64080ec7e6a173eb062e523ba873bc663473e979
Maximilian Sachs <Maximilian.Lukas.Sachs@outlook.de>
2024-08-07 21:20:42 +0200

    added CategoryArticle + FreshArticle

--- src/main/kotlin/com/janxi/homemanager/database/models/FreshArticle.kt
+++ src/main/kotlin/com/janxi/homemanager/database/models/FreshArticle.kt
@@ -1,4 +1,25 @@
 package com.janxi.homemanager.database.models
 
-class FreshArticle {
-}
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.math.BigDecimal
+import java.util.*
+
+
+@Entity
+class FreshArticle(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = -1,
+    val name: String,
+    val amount: Int,
+    val brand: String,
+    val price: BigDecimal,
+    @Column(name = "bought_on")
+    val boughtOn: Date,
+    val origin: String,
+    val category: String,
+)
