f1bcf7ef74eb3c19fe01d1fdcac32d234d7f1269
JantjeJoppien <jantje.joppien@gmail.com>
2024-08-07 20:11:39 +0200

    Init spring

---
+++ src/main/kotlin/com/janxi/homemanager/HomemanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.janxi.homemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomemanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomemanagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/janxi/homemanager/HomemanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.janxi.homemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomemanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
