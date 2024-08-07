8db897f3181255ff52735ee85d2b37995cddf32b
JantjeJoppien <jantje.joppien@gmail.com>
2024-08-07 21:50:31 +0200

    Small cleanup

--- src/main/kotlin/com/janxi/homemanager/database/models/FreshArticle.kt
+++ src/main/kotlin/com/janxi/homemanager/database/models/FreshArticle.kt
@@ -1,14 +1,6 @@
 package com.janxi.homemanager.database.models
 
-import jakarta.persistence.CollectionTable
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToOne
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.util.*
 
